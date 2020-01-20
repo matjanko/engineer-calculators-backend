081ce094db6376cc70a45a4c58b15f85b3f59054
matjanko <mateusz.jankowski@prokonto.pl>
2020-01-20 23:53:48 +0100

    Initial commit

---
+++ src/main/java/com/github/matjanko/calculators/EngineerCalculatorsApplication.java
@@ -0,0 +1,13 @@
+package com.github.matjanko.calculators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineerCalculatorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineerCalculatorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/matjanko/calculators/EngineerCalculatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matjanko.calculators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineerCalculatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
