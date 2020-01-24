c2e6bef22117e83efc39ddb8255238df1a0c347d
matjanko <mateusz.jankowski@prokonto.pl>
2020-01-24 19:03:30 +0100

    Security configuration
    * Permitted access for paths starting with '/api'

---
+++ src/main/java/com/github/matjanko/calculators/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.github.matjanko.calculators.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author matjanko
+ *
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/**").permitAll();
+    }
+}
