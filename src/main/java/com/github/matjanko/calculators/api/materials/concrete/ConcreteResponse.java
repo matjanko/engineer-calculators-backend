8ce2ff57d95cd41212eb559f762c3dd1f1c24e9e
matjanko <mateusz.jankowski@prokonto.pl>
2020-01-24 22:13:59 +0100

    'ConcreteResponse' data transfer object

---
+++ src/main/java/com/github/matjanko/calculators/api/materials/concrete/ConcreteResponse.java
@@ -0,0 +1,30 @@
+package com.github.matjanko.calculators.api.materials.concrete;
+
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author matjanko
+ *
+ */
+
+@Getter
+@Setter(AccessLevel.PACKAGE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ConcreteResponse {
+
+    @EqualsAndHashCode.Include
+    private String name;
+
+    private int fck;
+    private int fckCube;
+    private int fcm;
+    private double fctm;
+    private double fctk5;
+    private double fctk95;
+    private double ecm;
+    private int weight;
+
+}
