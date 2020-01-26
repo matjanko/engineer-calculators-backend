7ddd7034d465230ced78f062da256c9a8d4841ad
matjanko <mateusz.jankowski@prokonto.pl>
2020-01-26 09:44:33 +0100

    'ExposureClass' enum

---
+++ src/main/java/com/github/matjanko/calculators/api/cover/ExposureClass.java
@@ -0,0 +1,8 @@
+package com.github.matjanko.calculators.api.cover;
+
+/**
+ * @author matjanko
+ */
+public enum ExposureClass {
+    XC0, XC1, XC2, XC3, XC4, XD1, XD2, XD3, XS1, XS2, XS3, XF1, XF2, XF3, XF4, XA1, XA2, XA3
+}
