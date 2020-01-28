e33b19f736a6bda172e80b77c8a01e9fa6e1e933
matjanko <mateusz.jankowski@prokonto.pl>
2020-01-28 21:13:08 +0100

    Refactor 'Materials'

--- src/main/java/com/github/matjanko/calculators/api/materials/base/Materials.java
+++ src/main/java/com/github/matjanko/calculators/api/materials/base/Materials.java
@@ -18,20 +18,9 @@ public class Materials {
     public static List<Concrete> getConcretes() {
         List<Concrete> concretes = new LinkedList<>();
 
-        concretes.add(new ConcreteImpl(ConcreteClass.C12_15));
-        concretes.add(new ConcreteImpl(ConcreteClass.C16_20));
-        concretes.add(new ConcreteImpl(ConcreteClass.C20_25));
-        concretes.add(new ConcreteImpl(ConcreteClass.C25_30));
-        concretes.add(new ConcreteImpl(ConcreteClass.C30_37));
-        concretes.add(new ConcreteImpl(ConcreteClass.C35_45));
-        concretes.add(new ConcreteImpl(ConcreteClass.C40_50));
-        concretes.add(new ConcreteImpl(ConcreteClass.C45_55));
-        concretes.add(new ConcreteImpl(ConcreteClass.C50_60));
-        concretes.add(new ConcreteImpl(ConcreteClass.C55_67));
-        concretes.add(new ConcreteImpl(ConcreteClass.C60_75));
-        concretes.add(new ConcreteImpl(ConcreteClass.C70_85));
-        concretes.add(new ConcreteImpl(ConcreteClass.C80_95));
-        concretes.add(new ConcreteImpl(ConcreteClass.C90_105));
+        for (ConcreteClass concreteClass : ConcreteClass.values()) {
+            concretes.add(new ConcreteImpl(concreteClass));
+        }
 
         return concretes;
     }
