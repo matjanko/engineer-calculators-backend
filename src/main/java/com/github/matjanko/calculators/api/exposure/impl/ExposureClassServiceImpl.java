9c073acf2ce5258982d143df1fd356520baebd6c
matjanko <mateusz.jankowski@prokonto.pl>
2020-01-28 23:46:28 +0100

    'ExposureClassService' implementation

--- src/main/java/com/github/matjanko/calculators/api/exposure/impl/ExposureClassServiceImpl.java
+++ src/main/java/com/github/matjanko/calculators/api/exposure/impl/ExposureClassServiceImpl.java
@@ -1,9 +1,12 @@
 package com.github.matjanko.calculators.api.exposure.impl;
 
+import com.github.matjanko.calculators.api.exposure.ExposureClass;
 import com.github.matjanko.calculators.api.exposure.ExposureClassService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author matjanko
@@ -15,6 +18,8 @@ public class ExposureClassServiceImpl implements ExposureClassService {
 
     @Override
     public List<String> getAllExposureClasses() {
-        return null;
+        return Stream.of(ExposureClass.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
     }
 }
